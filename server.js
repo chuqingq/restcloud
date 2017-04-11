2c7cc8d9b246e0456e4f2d6f6ef58c83724fcc77
chuqq <chuqingq@qq.com>
2017-04-11 19:16:25 +0800

    get and save

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 // 1. 先设置支持跨域
-
 // 2. 拼接一个collection.json，用newman.run(json, {reporter: 'json'})来获取结果，并返回
+// 去掉对json-server的依赖，自己读写文件
+
+var fs = require('fs');
 
 var express = require('express');
-var jsonServer = require('json-server');
 var bodyParser = require('body-parser');
 var newman = require('newman');
 
-var jsonServerRouter = jsonServer.router('./LFS.postman_collection.json');
+const COLLECTION_JSON = './LFS.postman_collection.json';
 
 var app = express();
 app.use(express.static('./'));
 app.use(bodyParser.json());
+// 获取数据
+app.get('/api/get', function(req, res) {
+    console.log('/api/get:');
+    var content = fs.readFileSync(COLLECTION_JSON);
+    res.send(content);
+});
+// 保存所有数据
+app.post('/api/saveall', function(req, res) {
+    console.log('/api/saveall: ');
+    // 需要Content-Type是application/json
+    var output = JSON.stringify(req.body, null, '  ');
+    console.log('output: ' + output);
+    fs.writeFileSync(COLLECTION_JSON, output);
+    res.send({});
+});
+// 运行一个请求
 app.post('/api/run', function(req, res) {
     // 请求的body是个request，响应的body是response
-    console.log('req.body: ' + JSON.stringify(req.body, '  ', null));
+    console.log('/api/run: ' + req.body.method + ' ' + req.body.name);
     var collection = {
         "variables": [],
         "info": {
@@ -26,7 +43,7 @@ app.post('/api/run', function(req, res) {
         "item": []
     };
     collection.item = [req.body];
-    console.log('==== collection: ' + JSON.stringify(collection, '  ', null));
+    // console.log('==== collection: ' + JSON.stringify(collection, null, '  '));
 
     newman.run({
         collection: collection,
@@ -39,6 +56,5 @@ app.post('/api/run', function(req, res) {
         res.send(res2.run.executions[0].response);
     });
 });
-app.use(jsonServerRouter);
 
 app.listen(8080);
