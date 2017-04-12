103d7c7464ea98224098f15f9831cdfcc8543bc2
chuqingq <chuqingq@qq.com>
2017-04-12 23:36:39 +0800

    user save and change his own collection

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var session = require('express-session')
 var bodyParser = require('body-parser');
 var newman = require('newman');
 
-const COLLECTION_JSON = './LFS.postman_collection.json';
+var config = require('./package.json').config;
 
 var app = express();
 app.use(bodyParser.json());
@@ -21,43 +21,83 @@ app.use(session({
     // cookie TODO 是否要设置maxAge
 }));
 
-app.post('/api/login', function(req, res) {
-    console.log('/api/login username: ' + req.body.username);
-    console.log('/api/login password: ' + req.body.password);
-    // TODO 校验用户
-    req.session.username = req.body.username;
-    req.session.save();
-    res.json({});
+app.post('/api/user/login', function(req, res) {
+    console.log('/api/user/login username: ' + req.body.username);
+    console.log('/api/user/login password: ' + req.body.password);
+    // 校验用户
+    for (var i = 0; i < config.user.length; i++) {
+        var user = config.user[i];
+        if (user.username == req.body.username) {
+            if (user.password == req.body.password) {
+                req.session.username = req.body.username;
+                break;
+            }
+            else {
+                break;
+            }
+        }
+    }
+    console.log('username: ' + req.session.username);
+    if (req.session.username) {
+        req.session.save();
+        res.json({
+            username: req.session.username,
+            collection: JSON.parse(fs.readFileSync(req.session.username+'_collection.json'))
+        });
+    } else {
+        res.json({ret: -1, msg: '用户名或密码无效'})
+    }
 });
-app.post('/api/logout', function(req, res) {
-    console.log('/api/logout username: ' + req.session.username);
+app.post('/api/user/logout', function(req, res) {
+    console.log('/api/user/logout username: ' + req.session.username);
     if (!req.session.username) {
-        console.log('/api/logout not longined');
+        console.log('/api/user/logout not longined');
         res.json({ret: 400, msg: 'not logined'});
         return;
     }
     req.session.username = false;
     res.json({});
 });
+app.get('/api/session', function(req, res) {
+    if (req.session.username) {
+        res.json({
+            username: req.session.username,
+            collection: JSON.parse(fs.readFileSync(req.session.username+'_collection.json'))
+        });
+    }
+    else {
+        res.json({ret:-1, msg: '会话无效'});
+    }
+});
 // 获取数据
-app.get('/api/get', function(req, res) {
-    console.log('/api/get:');
-    var content = fs.readFileSync(COLLECTION_JSON);
+app.get('/api/collection/get', function(req, res) {
+    console.log('/api/collection/get:');
+    if (!req.session.username) {
+        res.send({ret:-1,msg:'会话失效'})
+        return;
+    }
+    var content = fs.readFileSync(req.session.username + '_collection.json');
+    // TODO 如果文件不存在，则回复一个空的模板
     res.send(content);
 });
 // 保存所有数据
-app.post('/api/saveall', function(req, res) {
-    console.log('/api/saveall: ');
+app.post('/api/collection/saveall', function(req, res) {
+    console.log('/api/collection/saveall: ');
     // 需要Content-Type是application/json
+    if (!req.session.username) {
+        res.json({ret:-1,msg:'会话失效'});
+        return;
+    }
     var output = JSON.stringify(req.body, null, '  ');
     console.log('output: ' + output);
-    fs.writeFileSync(COLLECTION_JSON, output);
-    res.send({});
+    fs.writeFileSync(req.session.username+'_collection.json', output);
+    res.json({});
 });
 // 运行一个请求
-app.post('/api/run', function(req, res) {
+app.post('/api/collection/run', function(req, res) {
     // 请求的body是个request，响应的body是response
-    console.log('/api/run: ' + req.body.method + ' ' + req.body.name);
+    console.log('/api/collection/run: ' + req.body.method + ' ' + req.body.name);
+    // TODO collection做成一个模板
     var collection = {
         "variables": [],
         "info": {
